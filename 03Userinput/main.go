8a31067d879a1a727e1348cf6fb536f2aa93ff6d
Rabindra Mishra <131529201+RabindraMishra-AIDS@users.noreply.github.com>
2024-12-24 23:40:20 +0530

    Userinput using os & bufio packages.

---
+++ 03Userinput/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	welcome := "Welcome our dear Users"
+	fmt.Println(welcome)
+	reader := bufio.NewReader(os.Stdin) //Note we are reading from os(Stdin) ie.StandardInput output.
+	fmt.Println("Enter the rating of the services: ")
+
+	//Comma ok|| error
+	//if error is there then it will get store in '_' varibale and output may get store in to input variable
+	//simply think of this that first part is try varibale and second part is catch variable.
+	input,_:=reader.ReadString('\n')
+	fmt.Println("Rating for our given service is",input)
+	fmt.Printf("Tye of Rating is:%T",input)  //But there is one problem here it is showing type of input is string.
+
+	//Note: If we are not concerned with input then istead of input we can also write like '_'.
+}
