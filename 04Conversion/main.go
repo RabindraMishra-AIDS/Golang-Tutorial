ba1bfd4cac4510544d8ce8239dcdc4ce6edc6c2e
Rabindra Mishra <sambhu311275@gmail.com>
2025-03-17 21:18:06 +0530

    working on string conversion

--- 04Conversion/main.go
+++ 04Conversion/main.go
@@ -11,7 +11,7 @@ func main()  {
 	fmt.Println("Welcome to our pizza App")
 	fmt.Println("Please rate us between 1 and 5")
 	reader:=bufio.NewReader(os.Stdin)
-	input,_:= reader.ReadString('\n')
+	input,_:= reader.ReadString('\n') //we want to read it uptil new line
 	fmt.Println("Rating received by user was:",input)
 
 	// numRating=input + 1 [in this it will throw error because we input is of type string. ReadString was used.]
