e5d8d4cb53d7e797ca802dfd2fbc1c3035452e12
Rabindra Mishra <sambhu311275@gmail.com>
2025-04-04 23:09:00 +0530

    completed Time in GO

--- 05Time/main.go
+++ 05Time/main.go
@@ -1,6 +1,7 @@
 package main
 
-import "fmt"
+import ("fmt"
+"time")
 
 func main() {
 	fmt.Println("Welcome to the Time package!")
@@ -8,4 +9,11 @@ func main() {
 	presentTime := time.Now() //Get current time
 
 	fmt.Println("Current Time: ",presentTime)
+
+	//We can also format time as per requirement
+	fmt.Println(presentTime.Format("01-02-2006 Monday")) //Note: This 01-02-2006 is unique way that GO follows to delcare a time format.
+
+	//Creating a Date 
+	createdDate:=time.Date(2021,time.April,10,23,0,0,0,time.UTC)
+	fmt.Println("createdDate:",createdDate)
 }
