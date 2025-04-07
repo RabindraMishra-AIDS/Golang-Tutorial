f0beea368aed54de477845aeba5ae6a63ac76d44
Rabindra Mishra <sambhu311275@gmail.com>
2025-04-07 14:57:27 +0530

    Part2: Slice Datatype

---
+++ 09slice2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var highscore = []int{10, 45, 78, 62}
+	fmt.Println("Highscore:", highscore)
+
+	//Removing a Value from Slice based on index
+	var programming = []string{"Python","Ruby","Swift","flutter"}
+	index:=2
+	programming=append(programming[:index],programming[index+1:]...) //Last index is excluded in slicing.
+	fmt.Println("Removed indexed 2:",programming)
+}
---
+++ 09slice2/main1.go
@@ -0,0 +1,5 @@
+package main
+
+func goconcept()  string{
+	return "Hello"
+}
