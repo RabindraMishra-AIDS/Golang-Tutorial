2c8adea676175de54febe22cd92e5b40deb60405
Rabindra Mishra <sambhu311275@gmail.com>
2025-05-23 13:05:06 +0530

    Url Handling in GO

---
+++ 20Url_Handling/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+const myurl string = "https://www.hindustantimes.com/world-news/pakistan-army-spokesman-channels-terrorist-hafiz-saeed-in-fresh-threat-to-india-if-you-block-our-water-101747959032272.html"
+
+func main() {
+	fmt.Println("Handling URL in Go")
+
+	//parsing the URL
+	result, _ := url.Parse(myurl)
+	fmt.Println(result)
+
+	//Extracting info  
+	fmt.Println(result.Scheme) 
+	fmt.Println(result.Host) 
+	fmt.Println(result.Path) 
+	fmt.Println(result.RawQuery) 
+	fmt.Println(result.Port()) 
+
+	queryParameters:=result.Query() //Query() Will store all the parameters of url in more better way.
+
+	fmt.Printf("The type of query params are:%T\n",queryParameters)
+
+	for _,val:= range queryParameters{
+		fmt.Println("Param is:",val)
+	} 
+
+	partsOfUrl:=&url.URL{ //Here we need to Pass Reference instead of actual copy
+		Scheme:"https",
+		Host:"www.hindustantimes.com",
+		Path:"/world-news/pakistan-army-spokesman-channels-terrorist-hafiz-saeed-in-fresh-threat-to-india-if-you-block-our-water-101747959032272.html",
+		RawPath:"Rabindra",
+
+	}
+	anotherURL:=partsOfUrl.String()
+	fmt.Println(anotherURL)
+}
